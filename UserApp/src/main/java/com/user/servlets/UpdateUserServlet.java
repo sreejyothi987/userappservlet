e0b62184fa1cee1db311e28041917a09ecf15bb5
jyothi udatha <jyothi.udatha@accenture.com>
2022-07-12 12:16:34 +0530

    updated

---
+++ UserApp/src/main/java/com/user/servlets/UpdateUserServlet.java
@@ -0,0 +1,68 @@
+package com.user.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateUserServlet
+ */
+@WebServlet("/updateServlet")
+public class UpdateUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+	public void init() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			 connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/new_db?useSSL=false","root","Jyo@@7989");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+	try {
+		Statement statement = connection.createStatement();
+		int result=statement.executeUpdate("update user set password+'"+password+"'where email='"+email+"'");
+		PrintWriter out = response.getWriter();
+		
+		if(result>0) {
+			
+		
+		
+		out.print("<H1> user created</H1>");
+		}
+		else {
+			out.print("<H1>ERROR CREATING THE USER</H1>");
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	
+	}
+	public void destroy() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
